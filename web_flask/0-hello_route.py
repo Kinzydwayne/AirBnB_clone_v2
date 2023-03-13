bf5492544598e09a6e9dc7ed4f33b6a78dd40e47
kinzydwayne <kinzydwaynecarter@gmail.com>
2023-03-13 19:03:31 +0000

    webflask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -18,3 +18,22 @@ def hello_hbnb():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
+#!/usr/bin/python3
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask("_name_")
+
+
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000, debug=False)
