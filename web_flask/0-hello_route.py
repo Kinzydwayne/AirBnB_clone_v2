f80620c84c7b0be2e4f909a30c4ca76845c0c68f
kinzydwayne <kinzydwaynecarter@gmail.com>
2023-03-14 11:37:48 +0000

    webflask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
 """
+This script starts a Flask web application and
+serves a "Hello HBNB!" message on the route /airbnb-onepage/.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/airbnb-onepage/')
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """
+    Defines a handler for the route /airbnb-onepage/.
+    Returns: "Hello HBNB!"
+    """
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
-#!/usr/bin/python3
-"""A script that starts a flask web application
-Your web application must be listening on 0.0.0.0, port 5000
-"""
-
-from flask import Flask
-
-app = Flask("_name_")
-
-
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
-
-
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
